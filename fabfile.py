943d68a814d72ae301bd352a46682be1dd38fe78
Max Countryman <max.countryman@gmail.com>
2010-09-28 22:08:45 -0400

    Adding some additional defs

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,8 @@ from fabric.api import *
 
 env.hosts = ['arkham.voxinfinitus.net']
 env.project_path = '/srv/python-environments/voxinfinitus'
-env.socket_path = '/tmp/cherokee/'
+env.socket_path = '/tmp/cherokee'
+env.memcached_path ='/etc/rc.d/memcached' #this should be the init
 
 def deploy():
     "Push local changes to server, pull changes on server, destroy our socket"
@@ -12,7 +13,15 @@ def deploy():
 
 def refresh_socket():
     "Refresh our socket, restart uWSGI"
-    run('rm %(socket_path)s/voxi-live.sock;' % env, pty=True)
+    run('rm %(socket_path)s/voxi-live.sock' % env, pty=True)
+
+def restart_memcached():
+    "Restart Memcached"
+    run('sudo %(memcached_path)s restart' % env, pty=True)
+
+def pip_requirements():
+    "Install requirements.txt"
+    run('pip -E voxinfinitus install -r %(project_path)s/requirements.txt' % env, pty=True)
 
 def ps_cherokee():
     run("ps -e -O rss,pcpu | grep cherokee")
