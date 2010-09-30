8ace74decb3673b96812279ea7ac1248ed67309b
Max Countryman <max.countryman@gmail.com>
2010-09-30 16:07:44 -0400

    fixing a few typos

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 from django.db import models
 from django.template.defaultfilters import slugify
@@ -11,13 +12,21 @@ class Author(models.Model):
 
 class Post(models.Model):
     title        = models.CharField(max_length=64)
-    slug         = models.SlugField(unique_for_date='date')
+    slug         = models.SlugField(unique_for_date='date_published')
     author       = models.ForeignKey(Author)
     body         = models.TextField(blank=True)
     tease        = models.TextField(blank=True, 
         help_text='A brief description of the post.')
-    published_on = models.DateField(auto_now_add=True)    
+    date_published = models.DateTimeField()
+    date_modified = models.DateTimeField(auto_now=True)
+    tags = models.CharField(maxlength=200)
+
+    def get_tag_list(self):
+        return re.split(" ", self.tags)    
     
     def __unicode__(self):
-        return "%s (%s)" % (self.title, self.author.name)    
+        return "%s (%s)" % (self.title,)
 
+    class Meta:
+        ordering = ["-date_published"]
+    
