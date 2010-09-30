245fde1b0e2f3410a458811477bac2c2172570d8
Max Countryman <max.countryman@gmail.com>
2010-09-30 08:49:09 -0400

    We need a few things from settings.py

---
+++ context_processors.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+def settings_context(request):
+    base_url = 'http%s://%s' % ('s' if request.is_secure() else '',
+                                request.get_host())
+
+    return {
+        'site_name': settings.SITE_NAME,
+        'site_description': settings.SITE_DESCRIPTION,
+        'site_copyright': settings.SITE_COPYRIGHT,
+        'site_base_url': base_url,
+        'google_analytics_id': getattr(settings, 'GOOGLE_ANALYTICS_ID', None),
+    }
