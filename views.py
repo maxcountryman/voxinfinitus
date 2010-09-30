73f94f04790626d968bb4a71fed0da5f5cfb47e4
Max Countryman <max.countryman@gmail.com>
2010-09-30 09:10:17 -0400

    context processors are nice

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def get_menulist():	
@@ -7,19 +8,19 @@ def get_menulist():
 def landing(request):
 	menulist = get_menulist()
 	curview = 'Home'
-	return render_to_response("index.html", locals())
+	return render_to_response("index.html", locals(), context_instance=RequestContext(request))
 
 def discover(request):
 	menulist = get_menulist()
 	curview = 'Discover'
-	return render_to_response("discover.html", locals())
+	return render_to_response("discover.html", locals(), context_instance=RequestContext(request))
 
 def services(request):
 	menulist = get_menulist()
 	curview = 'Services'
-        return render_to_response("services.html", locals())
+        return render_to_response("services.html", locals(), context_instance=RequestContext(request))
 
 def participate(request):
 	menulist = get_menulist()
 	curview = 'Participate'
-        return render_to_response("participate.html", locals())
+        return render_to_response("participate.html", locals(), context_instance=RequestContext(request))
