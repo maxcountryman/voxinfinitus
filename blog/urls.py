f04177061072dfb21351ba8fbfa35113c0c43329
Max Countryman <max.countryman@gmail.com>
2010-10-02 12:44:56 -0400

    trying some things

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('blog.views',
     (r'^(?P<year>\d\d\d\d)/(?P<month>\d\d)/(?P<post_url>.+)$', 'show'),
-    (r'^tag/(?P<tag>[a-z0-9_-]+)/$', 'browse_tagged_posts'),
+    (r'^tag/(?P<tag>.+)$', 'browse_tagged_posts'),
     (r'^$', 'browse'),
 )
