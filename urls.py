b96220bebca2446122956196adb755d5594668e6
Max Countryman <max.countryman@gmail.com>
2010-10-02 21:36:44 -0400

    updating

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-exec "from %s import views" % settings.PROJECT_DIR
 import blog
+import taggit
+exec "from %s import views" % settings.PROJECT_DIR
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,9 +10,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
       (r'^admin/doc/', include('django.contrib.admindocs.urls')),
       (r'^admin/', include(admin.site.urls)),
-
-      (r'articles/', include('blog.urls')),
-
+      (r'^comments/', include('django.contrib.comments.urls')),
+      (r'^articles/', include('blog.urls')),
+      (r'^articles/tag/(?P<tag>[^/]+)/$', 'taggit.views.tagged_object_list', 
+            dict(slug='tag', queryset=blog.Post.objects.all())),
       (r'^$', 'views.landing'),
 )
-
