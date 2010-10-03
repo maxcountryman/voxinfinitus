db09ab1f9e7349569e6c84dc8a4bb13573341ad9
Max Countryman <max.countryman@gmail.com>
2010-10-02 20:22:08 -0400

    cleaning up

--- views.py
+++ views.py
@@ -2,10 +2,5 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-def get_menulist():	
-	return {'Home': '/', 'Discover': '/discover/', 'Services': '/services/', 'Participate': '/participate/'}
-
 def landing(request):
-	menulist = get_menulist()
-	curview = 'Home'
 	return render_to_response("index.html", locals(), context_instance=RequestContext(request))
