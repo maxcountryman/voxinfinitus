1fe48709dcf90478603fb5fa4922af3da206d2fa
Max Countryman <max.countryman@gmail.com>
2010-10-15 22:39:25 -0400

    fixing a typo, missed a slash

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from blog.views import RssNewsFeed, AtomNewsFeed
 
 urlpatterns = patterns('blog.views',
     (r'^(?P<blog_url>[^/]+)/(?P<year>\d\d\d\d)/(?P<month>\d\d)/(?P<post_url>.+)$', 'show'),
-    (r'^(?P<blog_url>[^/]+)$', 'browse'),
+    (r'^(?P<blog_url>[^/]+)/$', 'browse'),
     (r'^(?P<blog_url>[^/]+)/rss/$', RssNewsFeed()),
     (r'^(?P<blog_url>[^/]+)/atom/$', AtomNewsFeed()),
 )
