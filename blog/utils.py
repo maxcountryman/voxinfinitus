260907c1e8ef49146a5b60be6463b4d37e3f2356
Max Countryman <max.countryman@gmail.com>
2010-10-18 21:19:15 -0400

    using smart_truncate for titles...it's smart

--- blog/utils.py
+++ blog/utils.py
@@ -2,15 +2,19 @@ import tweepy
 import bitly_api
 from django.conf import settings
 
+def smart_truncate(content, length=64, suffix='...'):
+    if len(content) <= length:
+        return content
+    else:
+        return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+
 def tweet(title, uri):
     bitly = bitly_api.Connection(settings.BITLY_USER, settings.BITLY_KEY) 
     short_url = bitly.shorten(uri)
     auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
     auth.set_access_token(settings.ACCESS_KEY, settings.ACCESS_SECRET)
     api = tweepy.API(auth)
-    if len(title) > 64:
-        title = title[:64] + "..."
-    title = title + ' -- '
+    title = smart_truncate(title) + ' -- '
     post_update = title + short_url['url']
     status = api.update_status(post_update)
     return status
